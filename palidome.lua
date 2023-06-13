5ab24b7dc89d39dc76b939d6d4558210db7e04c0
Senz <99367113+fsenzol@users.noreply.github.com>
2023-06-13 03:23:59 +0100

    palidome.lua

--- palidome.lua
+++ palidome.lua
@@ -1,10 +1,12 @@
-local function isPalidome(str)
-local str1 = str:gsub('[%p%s]','')
-if string.reverse(str1) == str1 then
-    return true
-else return false
-end
-end
+-- Checks if a word is palidome
 
-print(type(isPalidome('madam')))
-io.write(type(tostring(isPalidome("madam"))))
+local function isPalidome(str)
+    local str1 = str:gsub('[%p%s]','') -- removes any punctuations or whitespaces
+        if string.reverse(str1) == str1 then 
+            return true
+        else 
+            return false
+        end
+    end
+
+io.write(isPalidome("madam")) -- true
