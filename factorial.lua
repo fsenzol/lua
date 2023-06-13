3d09ab17336b73cefba1816a7bb291918fc3b9e4
Senz <99367113+fsenzol@users.noreply.github.com>
2023-06-13 03:32:55 +0100

    factorial.lua

--- factorial.lua
+++ factorial.lua
@@ -1,28 +1,16 @@
+-- A simple factorial program in lua using recursion
+
 local function factorial(n)
-    if n == 0 then
-        return 1
-    end
+        if n == 0 then
+            return 1
+        end
         if n < 0 then 
             return nil
-     end
-return n * factorial(n-1)
-    end
+        end
+    return n * factorial(n-1) 
+end
 
-    local function comb(m,n)
-        return factorial(m)/((factorial(m-n))*factorial(n))
-    end 
-        
-    local function shuffle(a)
-        local n = factorial(#a)
-    end
-    math.randomseed(os.time())
-   _G.arr = {}
+io.write(factorial(3)) --> 6 
 
-   for i = 1, 10 do
-    arr[i] = math.random(10)
-    while arr[i] == math.random(10) do
-        arr[i] = math.random(10)
-    end
-    
-    print(arr[i])
-   end
+
+ 
