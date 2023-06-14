4616c222ccffe21698db718357f7a88f8fb39116
fsenzol <tazuhf@gmail.com>
2023-06-14 07:56:41 +0100

    Added decimal2bin.lua

---
+++ decimal2bin.lua
@@ -0,0 +1,19 @@
+-- A function to convert any binary number, to its decimal equivalence
+
+
+local function dec2bin(bin)
+    local length = #tostring(bin)
+    local result = 1
+        if (bin == 0 or bin == 1) then
+            return bin
+        end
+        if( tostring(bin):match("[^10]") ) then
+            return "Invalid Binary Number!"
+        end
+        for i = 1, length - 1 do
+            result = result  + (2^i * tonumber(string.sub(bin, i, i))) 
+        end 
+    return result | 0  -- Converting it to an integer
+end
+
+print(dec2bin(1111))
