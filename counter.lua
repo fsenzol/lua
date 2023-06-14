c16965b4eed5ad6b23e7f47c0a490750a2b867d5
fsenzol <tazuhf@gmail.com>
2023-06-14 08:20:56 +0100

    counter.lua

---
+++ counter.lua
@@ -0,0 +1,14 @@
+-- A function that increments a counter, whenever called
+
+local function counter()
+    local counter = 0
+        return function()
+            counter = counter + 1
+            return counter
+        end
+end
+
+local counter = counter()
+print(counter()) --> 1
+counter()
+print(counter()) --> 3
